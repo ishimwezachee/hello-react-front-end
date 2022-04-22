3cef62335625d73630920342ed7c5b3c773d6c95
ishimwezachee <zacheeishimwe@gmail.com>
2022-04-22 16:40:30 +0200

    add ui features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router,Route,Routes}  from 'react-router-dom';
+import HelloWorld from './components/HelloWorld'
+import configureStore from "./Redux/configureStore";
+import { Provider } from "react-redux";
+const store = configureStore();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render () {
+    return (
+      <Provider store={store}>
+      <Router>
+         <Routes>
+           <Route path="/" element={<HelloWorld/>} />
+         </Routes>
+       </Router>
+       </Provider>
+    );
+  }
 }
 
-export default App;
+export default App
